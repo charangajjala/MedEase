f50ad34dd2a3a0afe6ec82b38e3f375776fdb6bf
G.C.Charan <charangajjala7@gmail.com>
2024-01-03 12:44:02 -0600

    initialized spring backend

---
+++ backend/src/main/java/com/chapp/med_ease/MedEaseApplication.java
@@ -0,0 +1,13 @@
+package com.chapp.med_ease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedEaseApplication.class, args);
+	}
+
+}
