3274d2a92200af9437b57d0511de76e08eae8e30
Prem <jampuramprem01@gmail.com>
2024-01-05 01:15:11 +0530

    added add medicine page and works fine just didn't any references and on submit functions

--- src/components/AddMedicine/AddMedicine.jsx
+++ src/components/AddMedicine/AddMedicine.jsx
@@ -6,11 +6,19 @@ import Sidebar from "../Sidebar/Sidebar";
 import ToggleButton from "../ToggleButton/ToggleButton";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import FormInput from "../FormInput/FormInput.jsx";
+import SelectField from "../SelectField/SelectField.jsx";
+
 import { links } from "../../constants/links.js";
+import { productTypes } from "../../constants/productTypes.js";
+import { companyNames } from "../../constants/companyNames.js";
 
 const AddMedicine = () => {
   const [isSidebarVisible, setIsSidebarVisible] = useState(true);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState("");
+  const [seletedCompany, setSelectedCompany] = useState("");
+
   const dropdownRef = useRef(null);
 
   const toggleSidebar = () => {
@@ -20,6 +28,14 @@ const AddMedicine = () => {
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
+
+  const handleSelectProduct = (e) => {
+    setSelectedProduct(e.target.value);
+  };
+
+  const handleSelectCompany = (e) => {
+    setSelectedCompany(e.target.value);
+  }
 
   return (
     <>
@@ -35,26 +51,101 @@ const AddMedicine = () => {
           isSidebarVisible={isSidebarVisible}
         />
         <main className="add-product-form__main">
-          <Header
-            dropdownRef={dropdownRef}
-            toggleDropdown={toggleDropdown}
-            isDropdownOpen={isDropdownOpen}
-            userName={"Admin"}
-            dropdownMenu={[
-              {
-                name: "Profile",
-                link: "/profile",
-              },
-              {
-                name: "Logout",
-                link: "/logout",
-              },
-            ]}
-            heading={"Add Medicine"}
-          />
+          <div className="add-product-form__main__header">
+            <Header
+              dropdownRef={dropdownRef}
+              toggleDropdown={toggleDropdown}
+              isDropdownOpen={isDropdownOpen}
+              userName={"Admin"}
+              dropdownMenu={[
+                {
+                  name: "Profile",
+                  link: "/profile",
+                },
+                {
+                  name: "Logout",
+                  link: "/logout",
+                },
+              ]}
+              heading={"Add Medicine"}
+            />
+          </div>
 
           <div className="add-product-form__content">
-            
+            <form className="form">
+              <SelectField
+                label="Select Product Type"
+                id="product-type"
+                name="product-type"
+                options={productTypes}
+                value={selectedProduct}
+                onChange={handleSelectProduct}
+                required={true}
+              />
+              <FormInput
+                label="Product Code"
+                type="text"
+                id="medicine-expiry"
+                name="medicine-expiry"
+                required={true}
+              />
+              <FormInput
+                label="Product Title"
+                type="text"
+                id="product-title"
+                name="product-title"
+                required={true}
+              />
+              <FormInput
+                label="Total Stock"
+                type="text"
+                id="total-stock"
+                name="total-stock"
+                required={true}
+              />
+              <SelectField
+                label="Select Company Name"
+                id="company-name"
+                name="company-name"
+                options={companyNames}
+                value={seletedCompany}
+                onChange={handleSelectCompany}
+                required={true}
+              />
+              <FormInput
+                label="Cost Per Month"
+                type="text"
+                id="cost-per-month"
+                name="cost-per-month"
+                required={true}
+              />
+              <FormInput
+                label="Expiry Date"
+                type="date"
+                id="expiry-date"
+                name="expiry-date"
+                required={true}
+              />
+              <FormInput
+                label="Manufacture Date"
+                type="date"
+                id="manufacture-date"
+                name="manufacture-date"
+                required={true}
+              />
+              <div className="grid-item full-width">
+                <label htmlFor="medicine-description">Description</label>
+                <textarea
+                  type="text"
+                  id="medicine-description"
+                  name="medicine-description"
+                  required
+                />
+              </div>
+              <div className="form-button">
+                <button type="submit">Submit</button>
+              </div>
+            </form>
           </div>
         </main>
       </div>
--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,8 +1,45 @@
+import PropTypes from "prop-types";
 
-const FormInput = () => {
+const FormInput = ({
+  label,
+  type,
+  id,
+  name,
+  value,
+  onChange,
+  required,
+  ...props
+}) => {
   return (
-    <div>FormInput</div>
-  )
-}
+    <div className="grid-item">
+      <label htmlFor={id}>{label}</label>
+      <input
+        type={type}
+        id={id}
+        name={name}
+        value={value}
+        onChange={onChange}
+        required={required}
+        {...props}
+      />
+    </div>
+  );
+};
 
-export default FormInput
+FormInput.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  required: PropTypes.bool.isRequired,
+};
+
+FormInput.defaultProps = {
+  type: "text",
+  required: false,
+  onChange: () => {},
+};
+
+export default FormInput;
