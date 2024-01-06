e32847c80230a53d7e6a286dc4b774046800a5ee
Prem <jampuramprem01@gmail.com>
2024-01-07 01:15:40 +0530

    Refactor AddMedicine component to use useReducer hook

--- src/containers/AddMedicine/AddMedicine.jsx
+++ src/containers/AddMedicine/AddMedicine.jsx
@@ -1,6 +1,6 @@
 import "./AddMedicine.scss";
 import logo from "../../assets/logo.png";
-import { useState } from "react";
+import { useReducer } from "react";
 
 import {
   Sidebar,
@@ -16,6 +16,43 @@ import { productTypes } from "../../constants/productTypes.js";
 import { companyNames } from "../../constants/companyNames.js";
 import useVisibilityToggle from "../../hooks/useVisibilityToggle.jsx";
 
+const initialState = {
+  productType: "",
+  productCode: "",
+  productTitle: "",
+  totalStock: "",
+  companyName: "",
+  costPerMonth: "",
+  expiryDate: "",
+  manufactureDate: "",
+  description: "",
+};
+
+function reducer (state, action) {
+  switch(action.type) {
+    case "SET_PRODUCT_TYPE":
+      return {...state, productType: action.payload};
+    case "SET_PRODUCT_CODE":
+      return {...state, productCode: action.payload};
+    case "SET_PRODUCT_TITLE":
+      return {...state, productTitle: action.payload};
+    case "SET_TOTAL_STOCK":
+      return {...state, totalStock: action.payload};
+    case "SET_COMPANY_NAME":
+      return {...state, companyName: action.payload};
+    case "SET_COST_PER_MONTH":
+      return {...state, costPerMonth: action.payload};
+    case "SET_EXPIRY_DATE":
+      return {...state, expiryDate: action.payload};
+    case "SET_MANUFACTURE_DATE":
+      return {...state, manufactureDate: action.payload};
+    case "SET_DESCRIPTION":
+      return {...state, description: action.payload};
+    default:
+      return state;
+  }
+}
+
 const AddMedicine = () => {
   const {
     isSidebarVisible,
@@ -25,23 +62,17 @@ const AddMedicine = () => {
     dropdownRef,
   } = useVisibilityToggle();
 
-  const [selectedProduct, setSelectedProduct] = useState("");
-  const [seletedCompany, setSelectedCompany] = useState("");
+  // const [selectedProduct, setSelectedProduct] = useState("");
+  // const [seletedCompany, setSelectedCompany] = useState("");
 
-  const handleSelectProduct = (e) => {
-    setSelectedProduct(e.target.value);
-  };
-
-  const handleSelectCompany = (e) => {
-    setSelectedCompany(e.target.value);
-  };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   // Once the backend is done convert it to async for post request
   // Upon successful submit clear the form
   // else display error message
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Submitted");
+    console.log(state);
   };
 
   return (
@@ -85,8 +116,8 @@ const AddMedicine = () => {
                 id="product-type"
                 name="product-type"
                 options={productTypes}
-                value={selectedProduct}
-                onChange={handleSelectProduct}
+                value={state.productType}
+                onChange={(e) => dispatch({type: "SET_PRODUCT_TYPE", payload: e.target.value})}
                 required={true}
               />
               <FormInput
@@ -94,6 +125,7 @@ const AddMedicine = () => {
                 type="text"
                 id="medicine-expiry"
                 name="medicine-expiry"
+                onChange={(e) => dispatch({type: "SET_PRODUCT_CODE", payload: e.target.value})}
                 required={true}
               />
               <FormInput
@@ -101,6 +133,7 @@ const AddMedicine = () => {
                 type="text"
                 id="product-title"
                 name="product-title"
+                onChange={(e) => dispatch({type: "SET_PRODUCT_TITLE", payload: e.target.value})}
                 required={true}
               />
               <FormInput
@@ -108,6 +141,7 @@ const AddMedicine = () => {
                 type="text"
                 id="total-stock"
                 name="total-stock"
+                onChange={(e) => dispatch({type: "SET_TOTAL_STOCK", payload: e.target.value})}
                 required={true}
               />
               <SelectField
@@ -115,8 +149,8 @@ const AddMedicine = () => {
                 id="company-name"
                 name="company-name"
                 options={companyNames}
-                value={seletedCompany}
-                onChange={handleSelectCompany}
+                value={state.companyName}
+                onChange={(e) => dispatch({type: "SET_COMPANY_NAME", payload: e.target.value})}
                 required={true}
               />
               <FormInput
@@ -124,6 +158,7 @@ const AddMedicine = () => {
                 type="text"
                 id="cost-per-month"
                 name="cost-per-month"
+                onChange={(e) => dispatch({type: "SET_COST_PER_MONTH", payload: e.target.value})}
                 required={true}
               />
               <FormInput
@@ -131,6 +166,7 @@ const AddMedicine = () => {
                 type="date"
                 id="expiry-date"
                 name="expiry-date"
+                onChange={(e) => dispatch({type: "SET_EXPIRY_DATE", payload: e.target.value})}
                 required={true}
               />
               <FormInput
@@ -138,6 +174,7 @@ const AddMedicine = () => {
                 type="date"
                 id="manufacture-date"
                 name="manufacture-date"
+                onChange={(e) => dispatch({type: "SET_MANUFACTURE_DATE", payload: e.target.value})}
                 required={true}
               />
               <div className="grid-item full-width">
@@ -146,6 +183,7 @@ const AddMedicine = () => {
                   type="text"
                   id="medicine-description"
                   name="medicine-description"
+                  onChange={(e) => dispatch({type: "SET_DESCRIPTION", payload: e.target.value})}
                   required
                 />
               </div>
