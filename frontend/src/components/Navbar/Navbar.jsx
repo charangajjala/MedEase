2b83533f4b09b9e19aa268e5ec7cd5496026ed7c
Prem <jampuramprem01@gmail.com>
2024-01-09 12:54:26 +0530

    Refactor Navbar component and styles

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,51 @@
-import './Navbar.scss';
+import PropTypes from "prop-types";
+import { FaUser, FaSignOutAlt } from "react-icons/fa";
+import "./Navbar.scss";
 
-const Navbar = () => {
+const Navbar = ({ userName, isDropdownOpen, toggleDropdown, dropdownMenu }) => {
   return (
     <nav className="navbar">
-      <div className="navbar__container">
-        <a href="/" id="navbar__logo">
-          Logo
-        </a>
-        <div className="navbar__toggle" id="mobile-menu">
-          <span className="bar"></span> <span className="bar"></span>
-          <span className="bar"></span>
-        </div>
+      <div className="navbar__brand">
+        <a href="/" className="navbar__logo">Logo</a>
+      </div>
+      <div className="navbar__menu">
+        {dropdownMenu.map((item, index) => (
+          <a href={item.link} key={index} className="navbar__menu-item">
+            {item.name}
+          </a>
+        ))}
+      </div>
+      <div className="navbar__user-info">
+        <button onClick={toggleDropdown} className="navbar__user-info-button">
+          {userName} <FaUser />
+        </button>
+        {isDropdownOpen && (
+          <ul className="navbar__user-info__dropdown-menu">
+            <li className="navbar__user-info__dropdown-menu-item">
+              <a href="/profile">Profile</a>
+            </li>
+            <li className="navbar__user-info__dropdown-menu-item">
+              <a href="/logout">
+                Logout <FaSignOutAlt />
+              </a>
+            </li>
+          </ul>
+        )}
       </div>
     </nav>
   );
+};
+
+Navbar.propTypes = {
+  userName: PropTypes.string.isRequired,
+  isDropdownOpen: PropTypes.bool.isRequired,
+  toggleDropdown: PropTypes.func.isRequired,
+  dropdownMenu: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      link: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default Navbar;
