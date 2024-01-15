1399ca64ecd25bdad1282fd7556e66ae0032cf07
Prem <jampuramprem01@gmail.com>
2024-01-15 19:01:50 +0530

    added handle search

--- frontend/src/userComponents/Navbar/Navbar.jsx
+++ frontend/src/userComponents/Navbar/Navbar.jsx
@@ -7,8 +7,32 @@ import {
   faLocation,
 } from "@fortawesome/free-solid-svg-icons";
 import PropTypes from "prop-types";
+import { useReducer } from "react";
 
-const Navbar = ({cartCount}) => {
+const initialState = {
+  category: "All Categories",
+  searchContent: "",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_CATEGORY":
+      return { ...state, category: action.payload };
+    case "SET_SEARCH_CONTENT":
+      return { ...state, searchContent: action.payload };
+    default:
+      throw new Error();
+  }
+}
+
+const Navbar = ({ cartCount }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { category, searchContent } = state;
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log(category, searchContent);
+  };
 
   return (
     <nav className="navbar-container">
@@ -26,11 +50,25 @@ const Navbar = ({cartCount}) => {
           id="categories"
           name="Categories"
           className="navbar-container__search__cat-search"
+          value={category}
+          onChange={(e) =>
+            dispatch({ type: "SET_CATEGORY", payload: e.target.value })
+          }
         >
           <option value="All Categories">All Categories</option>
         </select>
-        <input type="text" placeholder="Search here..." />
-        <button className="navbar-container__search-button">
+        <input
+          type="text"
+          placeholder="Search here..."
+          value={searchContent}
+          onChange={(e) =>
+            dispatch({ type: "SET_SEARCH_CONTENT", payload: e.target.value })
+          }
+        />
+        <button
+          className="navbar-container__search-button"
+          onClick={(e) => handleSearch(e)}
+        >
           <FontAwesomeIcon icon={faMagnifyingGlass} />
         </button>
       </div>
@@ -40,11 +78,7 @@ const Navbar = ({cartCount}) => {
         </button>
         <button className="navbar-container__cart-button">
           <FontAwesomeIcon icon={faShoppingCart} />
-          {cartCount > 0 && (
-            <span className="cart-count">
-              {cartCount}
-            </span>
-          )}
+          {cartCount > 0 && <span className="cart-count">{cartCount}</span>}
         </button>
       </div>
     </nav>
