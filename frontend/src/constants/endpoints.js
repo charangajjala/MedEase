4143a3b92f7b39cb88ab33ac55a72b9454414878
Prem <jampuramprem01@gmail.com>
2024-01-19 09:26:30 +0530

    updated endpoints and cart structure

--- frontend/src/constants/endpoints.js
+++ frontend/src/constants/endpoints.js
@@ -3,27 +3,27 @@ const Endpoints = {
   REGISTER: "/api/auth/register",
 
   // Product Related URLs
-  PRODUCT_REPORTS_URL: "/api/medicine",
-  UPDATE_PRODUCTS_URL: "/api/medicine",
+  PRODUCT_REPORTS_URL: "/api/admin/medicine",
+  UPDATE_PRODUCTS_URL: "/api/admin/medicine",
 
   // Company Related URLs
-  COMPANY_REPORTS_URL: "/api/company",
-  ADD_COMPANY_URL: "/api/company",
-  UPDATE_COMPANY_URL: "/api/company",
+  COMPANY_REPORTS_URL: "/api/admin/company",
+  ADD_COMPANY_URL: "/api/admin/company",
+  UPDATE_COMPANY_URL: "/api/admin/company",
 
   // Category Related URLs
-  ADD_CATEGORY_URL: "/api/medicine_type",
-  GET_ONE_CATEGORY_URL: "/api/medicine_type/{id}",
-  GET_CATERGORY_URL: "/api/medicine_type",
+  ADD_CATEGORY_URL: "/api/admin/medicine_type",
+  GET_ONE_CATEGORY_URL: "/api/admin/medicine_type/{id}",
+  GET_CATERGORY_URL: "/api/admin/medicine_type",
 
   // Medicine Related URLs
-  ADD_MEDICINE_URL: "/api/medicine",
-  GET_ONE_MEDICINE_URL: "/api/medicine/{id}",
-  MEDICINE_REPORTS_URL: "/api/medicines",
-  UPDATE_MEDICINE_URL: "/api/medicines",
+  ADD_MEDICINE_URL: "/api/admin/medicine",
+  GET_ONE_MEDICINE_URL: "/api/admin/medicine/{id}",
+  MEDICINE_REPORTS_URL: "/api/admin/medicines",
+  UPDATE_MEDICINE_URL: "/api/admin/medicines",
 
   // Order Related URLs
-  ORDER_REPORTS_URL: "/api/orders",
+  ORDER_REPORTS_URL: "/api/admin/orders",
 
   // Login
   LOGIN_URL: "/api/auth/login",
