3d0501348bc762b1cead316a1afc0d3d5cb54b57
Prem <jampuramprem01@gmail.com>
2024-01-21 03:43:41 +0530

    Add chart.js and react-chartjs-2 dependencies

--- frontend/src/constants/orders.js
+++ frontend/src/constants/orders.js
@@ -15,6 +15,38 @@ const orders = [
       },
     ],
   },
+  {
+    orderId: 512,
+    orderDate: "13 January 2024 03:18 PM",
+    customerName: "John Does",
+    customerMobile: "9391098117",
+    totalSum: "18.00",
+    orders: [
+      {
+        id: 3,
+        totalUnits: "6",
+        productName: "Moov",
+        pricePerUnit: 10,
+        totalCost: 60,
+      },
+    ],
+  },
+  {
+    orderId: 512,
+    orderDate: "13 January 2023 03:18 PM",
+    customerName: "John Does",
+    customerMobile: "9391098117",
+    totalSum: "18.00",
+    orders: [
+      {
+        id: 3,
+        totalUnits: "6",
+        productName: "Moov",
+        pricePerUnit: 10,
+        totalCost: 60,
+      },
+    ],
+  },
 ];
 
 export default orders;
--- frontend/src/containers/Admin/AdminDashboard/AdminDashboard.jsx
+++ frontend/src/containers/Admin/AdminDashboard/AdminDashboard.jsx
@@ -1,5 +1,4 @@
 import "./AdminDashboard.scss";
-import store from "../../../assets/store.jpg";
 import logo from "../../../assets/logo.png";
 
 import {
@@ -11,6 +10,32 @@ import {
 
 import { links } from "../../../constants/links.js";
 import useVisibilityToggle from "../../../hooks/useVisibilityToggle.jsx";
+import { useEffect, useState } from "react";
+import useAxiosPrivate from "../../../hooks/useAxiosPrivate.jsx";
+import endpoints from "../../../constants/endpoints.js";
+import orders from "../../../constants/orders.js";
+
+import { Bar, Pie } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement
+);
 
 const AdminDashboard = () => {
   const {
@@ -20,6 +45,103 @@ const AdminDashboard = () => {
     toggleDropdown,
     dropdownRef,
   } = useVisibilityToggle();
+  const axiosPrivate = useAxiosPrivate();
+  const [products, setProducts] = useState([]);
+  const [companies, setCompanies] = useState([]);
+  const [productTypes, setProductTypes] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axiosPrivate.get(endpoints.PRODUCT_REPORTS_URL);
+        const data = await response.data;
+        setProducts(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    const fetchCompanies = async () => {
+      try {
+        const response = await axiosPrivate.get(endpoints.COMPANY_REPORTS_URL);
+        const data = await response.data;
+        setCompanies(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    const fetchProductTypes = async () => {
+      try {
+        const response = await axiosPrivate.get(endpoints.GET_CATERGORY_URL);
+        const data = await response.data;
+        setProductTypes(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchProducts();
+    fetchCompanies();
+    fetchProductTypes();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    return date.toLocaleDateString("en-US", {
+      year: "numeric",
+    });
+  }
+
+  const revenueByDate = {};
+  orders.forEach((order) => {
+    const formattedDate = formatDate(order.orderDate);
+    revenueByDate[formattedDate] =
+      (revenueByDate[formattedDate] || 0) + parseFloat(order.totalSum);
+  });
+
+  const orderBarChartData = {
+    labels: Object.keys(revenueByDate),
+    datasets: [
+      {
+        label: "Total Revenue ($)",
+        data: Object.values(revenueByDate),
+        backgroundColor: "rgba(153, 102, 255, 0.5)",
+      },
+    ],
+  };
+
+  const orderFrequency = {};
+  orders.forEach((order) => {
+    orderFrequency[order.customerName] =
+      (orderFrequency[order.customerName] || 0) + 1;
+  });
+
+  const pieChartData = {
+    labels: Object.keys(orderFrequency),
+    datasets: [
+      {
+        label: "Order Frequency",
+        data: Object.values(orderFrequency),
+        backgroundColor: ["rgba(255, 99, 132, 0.5)", "rgba(54, 162, 235, 0.5)"],
+      },
+    ],
+  };
+
+  const barChartData = {
+    labels: productTypes.map((type) => type.categoryName),
+    datasets: [
+      {
+        label: "Number of Products",
+        data: productTypes.map(
+          (type) =>
+            products.filter((p) => p.category === type.categoryName).length
+        ),
+        backgroundColor: "rgba(54, 162, 235, 0.5)",
+      },
+    ],
+  };
 
   return (
     <>
@@ -51,22 +173,29 @@ const AdminDashboard = () => {
                 link: "/logout",
               },
             ]}
-            heading={"Medical Store Management System Admin Dashboard"}
+            heading={"Medical Store Stats"}
           />
 
           <div className="admin-dashboard__content">
-            <div className="admin-dashboard__content-right">
-              {Object.values(links).map((link, index) => (
-                <a
-                  href={link.href}
-                  key={index}
-                  className="admin-dashboard__nav-link"
-                >
-                  {link.name}
-                </a>
-              ))}
-            </div>
-            <img src={store} alt="store" />
+            <section className="dashboard-section">
+              <h3>Products by Category</h3>
+              <Bar data={barChartData} />
+            </section>
+
+            <section className="dashboard-section">
+              <h3>Revenue by Customer</h3>
+              <Bar data={orderBarChartData} />
+            </section>
+
+            <section className="dashboard-section">
+              <h3>Order Frequency</h3>
+              <Pie data={pieChartData} />
+            </section>
+
+            <section className="dashboard-section">
+              <h3>Order Frequency</h3>
+              <Pie data={pieChartData} />
+            </section>
           </div>
         </main>
       </div>
