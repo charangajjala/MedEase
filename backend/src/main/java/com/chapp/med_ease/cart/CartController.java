c5ddeb358b4fb30957351fa1e019d0c45ea816bd
G.C.Charan <charangajjala7@gmail.com>
2024-01-22 17:32:35 -0600

    delete cart

--- backend/src/main/java/com/chapp/med_ease/cart/CartController.java
+++ backend/src/main/java/com/chapp/med_ease/cart/CartController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,8 +48,16 @@ public class CartController {
     }
 
     @PutMapping("/cartitem/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public void deleteCartItem(@PathVariable int id) throws BadRequestException {
         cartService.deleteCartItem(id);
+
+    }
+
+    @DeleteMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteCart() throws BadRequestException {
+        cartService.deleteCart();
 
     }
 
